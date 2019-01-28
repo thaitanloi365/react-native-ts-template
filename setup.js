249b960f54476d667265ecdf48495dd5a990cee0
Thai Tan Loi <thai.tan.loi@styl.solutions>
2019-01-28 11:04:38 +0700

    add scripts

--- setup.js
+++ setup.js
@@ -1,45 +1,70 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
-const packageJson = require('./package.json')
+const updatePackageJson = () => {
+  try {
+    let packageJson = require("./package.json");
+    const rnpm = {
+      assets: ["Sources/Assets/Fonts"]
+    };
+    packageJson.scripts.tsc = "tsc";
+    packageJson.scripts.android =
+      "adb reverse tcp:8081 tcp:8081 & react-native run-android";
+    packageJson.scripts.shake = "adb shell input keyevent 82";
+    packageJson.scripts.iosD = "react-native run-ios --device";
+    packageJson.scripts.iosE = "react-native run-ios";
+    packageJson = Object.assign(packageJson, { rnpm });
+  } catch (e) {
+    console.log("update package json error: ", e);
+  }
+};
 
-const deleteFile = fileName => fs.unlinkSync(path.join(__dirname, fileName))
-const writeFile = (fileName, data) => fs.writeFileSync(path.join(__dirname, fileName), data)
-const moveFile = (oldPath,newPath) => {
-    fs.rename(oldPath, newPath, function (err) {
-        if (err) {
-            if (err.code === 'EXDEV') {
-                copy();
-            } else {
-                console.log("rename error", err);
-            }
-            return;
-        }
+const deleteFile = fileName => {
+  try {
+    fs.unlinkSync(path.join(__dirname, fileName));
+  } catch (e) {
+    console.log("delete file error: ", fileName);
+  }
+};
+
+const writeFile = (fileName, data) =>
+  fs.writeFileSync(path.join(__dirname, fileName), data);
+
+const moveFile = (oldPath, newPath) => {
+  fs.rename(oldPath, newPath, function(err) {
+    if (err) {
+      if (err.code === "EXDEV") {
+        copy();
+      } else {
+        console.log("rename error: ", newPath);
+      }
+      return;
+    }
+  });
+
+  function copy() {
+    var readStream = fs.createReadStream(oldPath);
+    var writeStream = fs.createWriteStream(newPath);
+
+    readStream.on("error", () => console.log("copy error"));
+    writeStream.on("error", () => console.log("write error"));
+
+    readStream.on("close", function() {
+      fs.unlink(oldPath, () => console.log("read error"));
     });
 
-    function copy() {
-        var readStream = fs.createReadStream(oldPath);
-        var writeStream = fs.createWriteStream(newPath);
+    readStream.pipe(writeStream);
+  }
+};
 
-        readStream.on('error',() => console.log("copy error"));
-        writeStream.on('error', () => console.log("write error"));
+console.log("🔄 Setting up...");
+updatePackageJson();
+deleteFile(".flowconfig");
+deleteFile("App.js");
+deleteFile("setup.js");
+moveFile(
+  path.join(__dirname, "app.json"),
+  path.join(__dirname, "Sources/App/app.json")
+);
 
-        readStream.on('close', function () {
-            fs.unlink(oldPath, () => console.log("read error"));
-        });
-
-        readStream.pipe(writeStream);
-    }
-}
-
-console.log('🔄 Setting up...')
-
-packageJson.scripts.tsc = 'tsc'
-
-writeFile('package.json', JSON.stringify(packageJson, null, 2))
-
-deleteFile('.flowconfig')
-deleteFile('App.js')
-deleteFile('setup.js')
-moveFile(path.join(__dirname,'app.json'),path.join(__dirname,'Sources/App/app.json'))
-console.log(`✅ Setup completed!`)
+console.log(`✅ Setup completed!`);
